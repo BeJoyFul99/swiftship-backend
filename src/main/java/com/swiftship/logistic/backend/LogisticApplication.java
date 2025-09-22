502c0b3eb2c7af83cf221e05cf0ee866b34795d4
joynerlee99 <funk9937@gmail.com>
2025-09-22 16:48:06 -0400

    fixing db connection

--- src/main/java/com/swiftship/logistic/backend/LogisticApplication.java
+++ src/main/java/com/swiftship/logistic/backend/LogisticApplication.java
@@ -1,13 +1,18 @@
 package com.swiftship.logistic.backend;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class LogisticApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LogisticApplication.class, args);
+        SpringApplication app = new SpringApplication(LogisticApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        String dbUrl = env.getProperty("spring.datasource.url");
+        System.out.println("Database URL from environment: " + dbUrl);
     }
 
 
